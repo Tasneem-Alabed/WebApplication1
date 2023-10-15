33433d0dc0e20caba8d853535d51a0c88d10ef40
Tasneem-Alabed <tsneemabedd@gmail.com>
2023-10-15 04:20:19 +0300

    done

---
+++ WebApplication1/Modles/DTO/AddDTO.cs
@@ -0,0 +1,13 @@
+﻿namespace WebApplication1.Modles.DTO
+{
+    public class AddDTO
+    {
+        public int ID { get; set; }
+
+        public string Name { get; set; }
+
+        public int Layout { get; set; }
+
+        public int AmenityId { get; set; }
+    }
+}
---
+++ WebApplication1/Modles/DTO/RoomDTO.cs
@@ -0,0 +1,13 @@
+﻿namespace WebApplication1.Modles.DTO
+{
+    public class RoomDTO
+    {
+        public int ID { get; set; }
+
+        public string Name { get; set; }
+
+        public int Layout { get; set; }
+
+        public List<AmenityDTO> Amenities { get; set; }
+    }
+}
